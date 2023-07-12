60be5baadfdd98217e2a21213d22ebaf63d7bfb0
HoyeonKim <cabinkhy@gm.gist.ac.kr>
2023-07-13 01:46:52 +0900

    first commit

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// main color
+// ignore: constant_identifier_names
+const PRIMARY_COLOR = Color(0xFF22A45D);
+// text color
+// ignore: constant_identifier_names
+const BODY_TEXT_COLOR = Color(0xFF868686);
+// text field backgroudn color
+// ignore: constant_identifier_names
+const INPUT_BG_COLOR = Color(0xFFFBFBFB);
+// text field border color
+// ignore: constant_identifier_names
+const INPUT_BORDER_COLOR = Color(0xFFF3F2F2);
